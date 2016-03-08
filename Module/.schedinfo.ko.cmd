9a8456d896ed537233775e07425b413c000289c1
Berk <mehmetgurcay@ku.edu.tr>
2016-03-08 14:19:08 +0200

    Relocation and compiled filesof module

---
+++ Module/.schedinfo.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/osboxes/Desktop/OS/Kush/Module/schedinfo.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/osboxes/Desktop/OS/Kush/Module/schedinfo.ko /home/osboxes/Desktop/OS/Kush/Module/schedinfo.o /home/osboxes/Desktop/OS/Kush/Module/schedinfo.mod.o
---
+++ Module/.tmp_versions/schedinfo.mod
@@ -0,0 +1,2 @@
+/home/osboxes/Desktop/OS/Kush/Module/schedinfo.ko
+/home/osboxes/Desktop/OS/Kush/Module/schedinfo.o
